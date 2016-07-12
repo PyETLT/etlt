7667a1905fae0cd85342c0b63210a5069b5a805e
Paul Water <p.r.water@setbased.nl>
2016-07-12 08:37:13 +0200

    Release: 0.0.12

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ here = path.abspath(path.dirname(__file__))
 setup(
     name='ETLT',
 
-    version='0.0.11',
+    version='0.0.12',
 
     description='Extract Transform Load Transform ',
     # long_description=long_description,
