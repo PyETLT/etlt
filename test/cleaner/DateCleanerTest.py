ee65a5a62b81e9807f3ab3fbc04aec4ed05d1fc1
Paul Water <p.r.water@setbased.nl>
2016-07-14 12:42:13 +0200

    Added unit tests.

---
+++ test/cleaner/DateCleanerTest.py
@@ -0,0 +1,52 @@
+import unittest
+
+from etlt.cleaner.DateCleaner import DateCleaner
+
+
+class DateCleanerTest(unittest.TestCase):
+    # ------------------------------------------------------------------------------------------------------------------
+    def _test(self, expected, dirty):
+        clean = DateCleaner.clean(dirty)
+        self.assertEqual(expected, clean)
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test01(self):
+        self._test('1966-04-10', '19660410')  # YYYYMMDD format.
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test02(self):
+        self._test('1966-04-10', '1966-04-10')  # YYYY-MM-DD format.
+        self._test('1971-07-01', '1971-7-1')  # YYYY-M-D format.
+        self._test('1966-04-10', '10-04-1966')  # DD-MM-YYYY format.
+        self._test('1971-07-01', '1-7-1971')  # D-M-YYYY format.
+        self._test('1966-04-10', '10-04-66')  # DD-MM-YY format.
+        self._test('1971-07-01', '1-7-71')  # D-M-YY format.
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test03(self):
+        self._test('1966-04-10', '1966.04.10')  # YYYY.MM.DD format.
+        self._test('1971-07-01', '1971.7.1')  # YYYY.M.D format.
+        self._test('1966-04-10', '10.04.1966')  # DD.MM.YYYY format.
+        self._test('1971-07-01', '1.7.1971')  # D.M.YYYY format.
+        self._test('1966-04-10', '10.04.66')  # DD.MM.YY format.
+        self._test('1971-07-01', '1.7.71')  # D.M.YY format.
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test04(self):
+        self._test('1966-04-10', '1966/04/10')  # YYYY/MM/DD format
+        self._test('1971-07-01', '1971/7/1')  # YYYY/M/D format.
+        self._test('1966-04-10', '10/04/1966')  # DD/MM/YYYY format.
+        self._test('1971-07-01', '1/7/1971')  # D/M/YYYY format.
+        self._test('1966-04-10', '10/04/66')  # DD/MM/YY format.
+        self._test('1971-07-01', '1/7/71')  # D/M/YY format.
+
+    # ------------------------------------------------------------------------------------------------------------------
+    def test05(self):
+        self._test('1966-04-10', '1966 04 10')  # YYYY MM DD format
+        self._test('1971-07-01', '1971 7 1')  # YYYY M D format.
+        self._test('1966-04-10', '10 04 1966')  # DD MM YYYY format.
+        self._test('1971-07-01', '1 7 1971')  # D M YYYY format.
+        self._test('1966-04-10', '10 04 66')  # DD MM YY format.
+        self._test('1971-07-01', '1 7 71')  # D M YY format.
+
+# ----------------------------------------------------------------------------------------------------------------------
